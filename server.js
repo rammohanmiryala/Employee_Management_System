0e996bae5818f6f23867206f03f21bad0118e5cf
ram <rammohan944@gmail.com>
2021-12-13 15:45:22 +1030

    added the data

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const inquirer = require('inquirer');
-const db = require('./config/connection');
+const figlet = require('figlet');
+
 const fs = require("fs");
 
 // Import and require mysql2
 const mysql = require('mysql2');
+const {
+  Console
+} = require('console');
 
 const PORT = process.env.PORT || 3306;
 const app = express();
@@ -15,116 +19,161 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 
-const employee = () =>
-  inquirer.prompt([{
+// connect database connection
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: 'root',
+    database: 'Employee_Management_System_db'
+  },
+  console.log(`Connected to the Employee_Management_System_db database.`)
+
+);
+employee()
+
+function employee() {
+  inquirer.prompt([{
       type: "list",
       name: "answers",
       message: "what whould you like to do?",
-      choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
-    }
-
-  ])
-  .then((answers) => {
-
-    if (answers === "view all departments") {
-
-    }
-    if (answers === "view all roles") {
-
-    }
-
-    if (answers === "view all employees") {
-
-    }
-
-    if (answers === "add a department") {
-
-    }
-
-    if (answers === "add a role") {
-
-    }
-
-    if (answers === "add an employee") {
-
-    }
-    if (answers === "update an employee role") {
-
-    }
+      choices: [
+        "view all departments",
+        "view all roles",
+        "view all employees",
+        "add a department",
+        "add a role",
+        "add an employee",
+        "update an employee role"
+      ]
 
 
+    }])
+    .then((userinput) => {
 
-    // Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
+      if (userinput.answers === "view all departments") {
 
-    } else {
+        viewAllDepartments();
 
-    }
+      }
+      if (userinput.answers === "view all roles") {
+
+        viewallroles();
+
+      }
+      if (userinput.answers === "view all employees") {
+
+        viewallemployees();
+
+      }
+      if (userinput.answers === "add a department") {
+
+        adddepartment();
+
+      }
+      if (userinput.answers === "add a role") {
+
+        addarole();
+
+      }
+
+
+
+
+    })
+
+}
+
+function viewAllDepartments() {
+  db.query(`SELECT id AS DepartmentID , department_name AS DepartmentName FROM employee_management_system_db.department`, (err, data) => {
+    if (err) throw err;
+    console.table(data);
+    employee()
   });
+}
+// THEN I am presented with the job title, role id, the department 
+// that role belongs to, and the salary for that role
+function viewallroles() {
+  db.query(`SELECT roles.id,title,department.department_name as DepartmentName ,salary
+  FROM employee_management_system_db.department  right JOIN  employee_management_system_db.roles
+  ON department.id = roles.department_id;`, (err, data) => {
+    if (err) throw err;
+    console.table(data);
+    employee()
+  });
+}
+
+// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+function viewallemployees() {
+  db.query(`SELECT first_name, last_name,r.title, r.salary,d.department_name,manager_id as manager
+  FROM employee_management_system_db.employee e LEFT JOIN employee_management_system_db.roles r ON e.role_id = r.id LEFT JOIN employee_management_system_db.department d ON d.id = r.department_id;
+  `, (err, data) => {
+    if (err) throw err;
+    console.table(data);
+    employee()
+  });
+}
+
+function adddepartment() {
+  inquirer.prompt([{
+      type: "input",
+      name: "answers",
+      message: " name of the department?"
+
+    }])
+    .then((userinput) => {
+      db.query('INSERT INTO department (department_name) VALUES (?)', (userinput.answers), (err, data) => {
+        if (err) throw err;
+        console.table(data);
+        console.log(userinput.answers + "is added to department table")
+        employee()
+      });
 
 
+    })
 
-employee();
+}
 
+function addarole() {
+  db.promise().query('SELECT department.id, department.name FROM department')
+    .then(([departmentdata]) => {
 
+      // const department = departmenttable
+      let departmentrole = departmentdata.map(({
+        id,
+        name
+      }) => ({
+        value: id,
+        name: name,
+        
+      }))
 
+    })
 
-
-
-
-
-
-
-
-
-// const employee = () => {
-//   inquirer.prompt(
-//       [{
-
-//           type: "list",
-//           name: "datainput",
-//           message: "what whould you like to do?",
-//           choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
-//         }
-
-
-//       ])
-//     .then((answers) => {
-//       // Use user feedback for... whatever!!
-//     })
-//     .catch((error) => {
-//       if (error.isTtyError) {
-//         // Prompt couldn't be rendered in the current environment
-//       } else {
-//         // Something else went wrong
-//       }
-
-//     })
-
-// }
-// 
-
-
-
-
-// Hardcoded query: DELETE FROM course_names WHERE id = 3;
-
-// db.query(`DELETE FROM course_names WHERE id = ?`, 3, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
-
-// // Query database
-// db.query('SELECT * FROM course_names', function (err, results) {
-//   console.log(results);
-// });
-
-// // Default response for any other request (Not Found)
-// app.use((req, res) => {
-//   res.status(404).end();
-// });
+   inquirer.prompt([{
+        type: "input",
+        name: "title",
+        message: "What is the title of this new role?"
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "what is the salary of new role?"
+      },
+      {
+        type: "list",
+        name: "department_id",
+        message: "what department role is in ?",
+        choices: departmentrole
+      }
+    ])
+    .then((userinputs) => {
+      db.query('INSERT INTO role(title, salary, department_id) VALUES (?, ?, ?);', [userinputs.title, userinputs.salary, userinputs.department_id], (err, results) => {
+        if (err) throw err;
+        console.log("new role added")
+        employee()
+      })
+    })
+}
